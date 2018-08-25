0b1b97e78eff5faa92511d0ff66f79cf59640dc2
Agus <agus@atadatos.es>
2018-08-25 18:04:08 +0200

    First step, just open the web

--- main.js
+++ main.js
@@ -1,29 +1,20 @@
 const Apify = require('apify');
-const typeCheck = require('type-check').typeCheck;
-
-// Definition of the input
-const INPUT_TYPE = `{
-    message: Maybe String,
-}`;
 
 Apify.main(async () => {
-    // Fetch the input and check it has a valid format
-    // You don't need to check the input, but it's a good practice.
     const input = await Apify.getValue('INPUT');
-    if (!typeCheck(INPUT_TYPE, input)) {
-        console.log('Expected input:');
-        console.log(INPUT_TYPE);
-        console.log('Received input:');
-        console.dir(input);
-        throw new Error('Received invalid input');
-    }
+    console.log('My input is:');
+    console.dir(input);
+    const { url } = input;
 
-    // Here's the place for your magic...
-    console.log(`Input message: ${input.message}`);
+    console.log('Launching Puppeteer...');
+    const browser = await Apify.launchPuppeteer();
+    const page = await browser.newPage();
+    await page.goto(url);
+    const pageTitle = await page.title();
+    await browser.close();
 
-    // Store the output
-    const output = {
-        message: `${input.message} Hello my friend!`
-    };
-    await Apify.setValue('OUTPUT', output)
+    const output = { pageTitle, url };
+    console.log('My output is:');
+    console.dir(output);
+    await Apify.setValue('OUTPUT', output);
 });
